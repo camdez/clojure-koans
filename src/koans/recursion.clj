c1a77f0256b3a4fdb56a88a17a6ad62986b97fcd
Cameron Desautels <camdez@gmail.com>
2012-03-20 02:04:41 -0500

    A solid start

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -1,20 +1,26 @@
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
+      acc
       (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (if (= (count coll) 1)
+    coll
+    (conj (recursive-reverse (subvec coll 1)) (nth coll 0))))
 
 (defn factorial [n]
-  __)
+  (loop [n   n
+         acc 1]
+    (if (= n 0)
+      acc
+      (recur (dec n) (* n acc)))))
 
 (meditations
   "Recursion ends with a base case"
